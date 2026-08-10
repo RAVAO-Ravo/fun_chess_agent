03c44fb3dc80819981ece149999e8663a14a19d8
RAVAO-Ravo <96021186+RAVAO-Ravo@users.noreply.github.com>
2026-08-11 01:05:17 +0200

    Add files via upload

---
+++ build/release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/hardm/Documents/Projects/00_coding/05_chess_game_(no_github)/tmp
+# Build directory: /mnt/c/Users/hardm/Documents/Projects/00_coding/05_chess_game_(no_github)/tmp/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test("chess_tests" "/mnt/c/Users/hardm/Documents/Projects/00_coding/05_chess_game_(no_github)/tmp/build/release/chess_tests")
+set_tests_properties("chess_tests" PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/hardm/Documents/Projects/00_coding/05_chess_game_(no_github)/tmp/CMakeLists.txt;140;add_test;/mnt/c/Users/hardm/Documents/Projects/00_coding/05_chess_game_(no_github)/tmp/CMakeLists.txt;0;")
